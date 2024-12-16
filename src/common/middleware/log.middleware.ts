afa6c139ee2c4f531b300607021dd29b6bf8e2e7
Harsh Dalsaniya <dalsania983@gmail.com>
2024-12-16 16:26:57 +0530

    development feat: pre-commit hook & response logs

--- src/common/middleware/log.middleware.ts
+++ src/common/middleware/log.middleware.ts
@@ -10,16 +10,21 @@ import { Observable, map } from 'rxjs';
 @Injectable()
 export class LoggerMiddleware implements NestMiddleware {
   use(req: any, res: any, next: () => void) {
-    // let request = null;
+    let request = null;
     switch (req.method) {
       case 'GET':
-        // request = req.query ?? req.params;
+        request = req.query ?? req.params;
+        break;
+      case 'POST':
+      case 'PUT':
+      case 'PATCH':
+        request = req.body;
         break;
       default:
         break;
     }
     console.log(
-      `\x1b[01m\x1b[95m[${new Date().toDateString()}] . \x1b[31m[Request] . \x1b[34m[${req.method}] . \x1b[32m[${req.url}] . \x1b[32m \x1b[0m`,
+      `\x1b[01m\x1b[95m[${new Date().toDateString()}] . \x1b[31m[Request] . \x1b[34m[${req.method}] . \x1b[32m[${req.url}] ${request ? '. \x1b[96m' + JSON.stringify(request) : ''} \x1b[0m`,
     );
     next();
   }
