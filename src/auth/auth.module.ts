abb809f65ccc602eaa57475df3104a73ad9f0e67
Harsh Dalsaniya <dalsania983@gmail.com>
2024-12-18 15:01:56 +0530

    development fix: imports and clean ups

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,14 +1,12 @@
 import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
-// import configuration from 'config/configuration';
-import { PassportModule } from '@nestjs/passport';
-import { ConfigModule, ConfigService } from '@nestjs/config';
 import { JWTStrategy } from './jwt.strategy';
 
-// const config = configuration();
 @Module({
   imports: [
     PassportModule.register({ defaultStrategy: 'jwt' }),
